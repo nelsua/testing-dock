d9a4858d92be2d07f63498ca907eca6afac2a0fd
nelson enriquez <nlsnenrique@yahoo.com>
2023-03-26 15:04:20 -0400

    working fine 2

--- src/main/java/TestingDock.java
+++ src/main/java/TestingDock.java
@@ -1,8 +1,11 @@
 public class TestingDock {
+    public static void hei(int x, int y){
+        System.out.println(x+y);
+    }
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("hello world"+"trtataffdd");
         System.out.println("hello world");
-        System.out.println("hello world"+"trtataffdd");
+        hei(2,3);
     }
 }
